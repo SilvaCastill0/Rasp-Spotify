9c368493eaa30e98b09f431fe17bb6dca9e00639
SilvaCastill0 <tistiansilva@gmail.com>
2026-09-05 17:30:39 +0200

    While loop in main

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from soloist import play, start
 
 def main():
 	start()
-	uid = read_rfid()
-	spotify_id = find_album(uid)
-	play(spotify_id)
+	
+	while True:
+		uid = read_rfid()
+		spotify_id = find_album(uid)
+		play(spotify_id)
 
 if __name__ == '__main__':
 	main()
